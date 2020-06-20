f5b6e2e6e015b365feee6d9b63c839fade4781f6
Nithin <nithinyell@gmail.com>
2020-06-20 17:03:51 +0530

    Initial commit

--- ARSample2/ViewController.swift
+++ ARSample2/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureLighting()
+        addChair()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ARConfig()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    private func ARConfig() {
+        let config = ARWorldTrackingConfiguration()
+        sceneView.session.run(config)
     }
 
-
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    func addChair() {
+        guard let chairScene = SCNScene(named: "chair.dae") else { return }
+           let chairNode = SCNNode()
+           let chairSceneChildNodes = chairScene.rootNode.childNodes
+               
+           for childNode in chairSceneChildNodes {
+               chairNode.addChildNode(childNode)
+           }
+               
+        chairNode.position = SCNVector3(0, 0, -0.5)
+        chairNode.scale = SCNVector3(0.5, 0.5, 0.5)
+        sceneView.scene.rootNode.addChildNode(chairNode)
+    }
 }
 
